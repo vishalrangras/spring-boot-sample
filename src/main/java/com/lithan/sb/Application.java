dc81f9343ccd2a15df8c3e2561255e06839226cb
vishalrangras <vishalrangras@yahoo.com>
2019-07-25 13:42:26 +0530

    Updated Application.java to have Hello World in it.

--- src/main/java/com/lithan/sb/Application.java
+++ src/main/java/com/lithan/sb/Application.java
@@ -1,13 +1,26 @@
 package com.lithan.sb;
 
+import java.text.MessageFormat;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	
+	private static final Log LOG = LogFactory.getLog(Application.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		String user;
+		if(args.length>0) {
+			user = args[0];
+		}else {
+			user = "Spring Boot";
+		}
+		LOG.info(MessageFormat.format("============= Hello {0} world! =============", user));
 	}
-
+	
 }
